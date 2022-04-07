2897440cff754e5acc1b72575076d6ddfc13c281
sanjayakenji <sanjaya.kenji15153@gmail.com>
2022-04-07 19:55:08 +0700

    Menghitung keuangan 4 bagian presentase

---
+++ Presentase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menghitungkeuangan
+{
+    class Presentase
+    {
+        public double persepuluhan(int dana)
+        {
+            return dana * 0.1;
+        }
+
+        public double keinginan(int dana)
+        {
+            return dana * 0.2;
+        }
+
+        public double investasi(int dana)
+        {
+            return dana * 0.3;
+        }
+
+        public double kebutuhan(int dana)
+        {
+            return dana * 0.4;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menghitungkeuangan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Presentase presentase = new Presentase();
+
+            Console.Write("Dana yang kamu punya = ");
+            int uang = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Persepuluhan : Rp. {0}", presentase.persepuluhan(uang));
+
+            Console.WriteLine("Keinginan : Rp. {0}", presentase.keinginan(uang));
+
+            Console.WriteLine("Investasi : Rp. {0}", presentase.investasi(uang));
+
+            Console.WriteLine("Kebutuhan : Rp. {0}", presentase.kebutuhan(uang));
+        }
+    }
+}
